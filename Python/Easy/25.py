8a03f02292cda957316146dc7e311279c609a27a
Rudra-121106 <rudra121106@gmail.com>
2024-10-05 12:39:43 -0700

    python

--- Python/Easy/25.py
+++ Python/Easy/25.py
@@ -1,3 +1,6 @@
-def add(a, b): 
-              return a + b 
-              result = add(1, 2) + add
+def add(a, b):
+    return a + b 
+result = add(1, 2) 
+print(result)
+              
+              
