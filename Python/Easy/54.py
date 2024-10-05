f81065d2bee6d647b6229bdc9bfa17fae62ed98b
Rudra-121106 <rudra121106@gmail.com>
2024-10-05 13:45:07 -0700

    python

--- Python/Easy/54.py
+++ Python/Easy/54.py
@@ -1,12 +1,13 @@
 
+import csv
 def read_csv(filename):
   data = []
-try:
-     with open(filename, 'r') as file:
-      reader = csv.reader(file)
-      for row in reader:
-        data.append(row)
-except FileNotFoundError:
-   print('CSV file not found')
- return 
-result = read_csv('data.csv') 
+  try:
+      with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+          data.append(row)
+  except FileNotFoundError:
+    print('CSV file not found')
+    return 
+  result = read_csv('data.csv') 
